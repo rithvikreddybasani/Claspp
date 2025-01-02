b4415f9d4e5a4f5a75d65f0b53215fa9ff4d883a
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2025-01-02 16:23:56 +0530

    Update Logs.jsx

--- client/src/components/Logs.jsx
+++ client/src/components/Logs.jsx
@@ -14,7 +14,7 @@ function Logs() {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.get("http://localhost:5000/log");
+      const response = await axios.get("https://claspp.onrender.com/log");
       setLogs(response.data);
     } catch (error) {
       setError("Error fetching logs.");
